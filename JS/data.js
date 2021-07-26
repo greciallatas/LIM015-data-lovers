28203defe61714aeb0547d24bb83ca7449bc5209
Grecia Llatas <greciastefanni96@gmail.com>
2021-07-26 13:01:53 -0500

    Updates

--- JS/data.js
+++ JS/data.js
@@ -1,25 +1,30 @@
-import data from '../data/ghibli/ghibli.js';
-
-export const filterData = (director) => {
-  // const filterResult = films.filter(film => film.director === director)
-   // console.log(director);
-  const filterResult = data.films.filter(film => film.director.includes(director))
+export const filterData = (data, director) => {
+  const filterResult = data.filter(film => film.director.includes(director));
+  // console.log(data, director, filterResult);
   return filterResult;
 }
 
-export const filterFilmsById = (id) => {
-  const filterFilmsResult = data.films.filter(film => film.id.includes(id));
-  // console.log(filterFilmsResult);
+export const filterFilmsById = (data, id) => {
+  const filterFilmsResult = data.filter(film => film.id.includes(id));
+  // console.log(data, id, filterFilmsResult);
   return filterFilmsResult;
 }
 
-export const sortDataAscending = () => {
-  const sortResult = data.films.sort((a,b) => (a.release_date > b.release_date ? 1 : -1));
-  console.log(sortResult);
+export const sortDataAscending = (data) => {
+  const sortResult = data.sort((a,b) => a.release_date - b.release_date);
+  // console.log(data, sortResult);
   return sortResult;
 }
 
-export const sortDataDescending = () => {
-  const sortResult = data.films.sort((a,b) => (b.release_date > a.release_date ? 1 : -1));
+export const sortDataDescending = (data) => {
+  const sortResult = data.sort((a,b) => b.release_date - a.release_date);
+  // console.log(data, sortResult);
   return sortResult;
+}
+
+export const sortTop = (data) => {
+  const topFive = data.filter(film => film.rt_score >= 96);
+  const topResult = topFive.sort((a, b) => b.rt_score - a.rt_score);
+  // console.log(data, topResult);
+  return topResult;
 }
